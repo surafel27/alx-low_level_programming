5a73c47d96b0b35f1f83100b5a348e7a1ec04ac4
surafel27 <surafelfekadu399@gmail.com>
2022-09-08 11:10:06 -0700

    printing file using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
-/*
- * main - Entry point
+/**
+ * main - print the string inside printf
  *
+ * Description: using the main fuction the program print sting inside
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
