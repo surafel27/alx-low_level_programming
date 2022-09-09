15d17dc7931b32331ed1e4252bdf80c4744782aa
surafel27 <surafelfekadu399@gmail.com>
2022-09-09 13:19:42 -0700

    lowercase alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print the string inside printf
+ *
+ * Description: using the main fuction the program print sting inside
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	return (0);
+}
