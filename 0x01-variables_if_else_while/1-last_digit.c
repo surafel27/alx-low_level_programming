c804b2a65391f224fdfdecaa1da5794e7f5d214b
surafel27 <surafelfekadu399@gmail.com>
2022-09-09 11:18:53 -0700

    C is fun

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - print the string inside the function
+ *
+ * Description: using the main fuction the program print sting inside
+ * Return: Always 0 (Success)
+ */
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+	char a[100] = "Last digit of";
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n > 5)
+	{
+		printf("%s%d and greater than 5\n", a, n);
+	}
+	else if (n == 0)
+	{
+		printf("%s%d and is 0\n", a, n);
+	}
+	else if (n < 6 && n != 0)
+	{
+
+		printf("%s%d and is less than 6 and not 0", a, n);
+	}
+	return (0);
+}
