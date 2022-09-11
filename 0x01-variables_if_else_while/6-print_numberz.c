5f77fc7e6c63ad74605003c15197c32576d5cc00
surafel27 <surafelfekadu399@gmail.com>
2022-09-11 08:11:31 -0700

    print the number 0 9 using putchar function

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - print the string inside the function
+ *
+ * Description: using the main fuction the program print sting inside
+ * Return: Always 0 (Success)
+ */
+/* more headers goes there */
+/* betty style doc for function main goes there */
+int main(void)
+{
+{
+	int num;
+
+	for (num = 48; num <= 57; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}
