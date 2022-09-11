35436d5fcbe0a0d2ddcdc53dd684daae34ac307f
surafel27 <surafelfekadu399@gmail.com>
2022-09-11 07:12:06 -0700

    alphabets excet q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - print the string inside printf
+ *
+ * Description: using the main fuction the program print sting inside
+ * Return: Always 0 (Success)
+ */
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch == 'e' || ch == 'q')
+		{
+			ch++;
+		}
+		else
+		{
+			putchar(ch);
+		}
+	}
+	printf("\n");
+	return (0);
+}
