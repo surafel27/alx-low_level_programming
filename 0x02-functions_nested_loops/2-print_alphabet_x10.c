558658dd402d77d53310978cfd26563af6f57833
surafel27 <surafelfekadu399@gmail.com>
2022-09-13 02:47:38 -0700

    low level programming

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,18 +7,16 @@
  */
 void print_alphabet_x10(void)
 {
-        int num = 0;
+	int num = 0;
 	char ch;
+
 	while (num < 10)
 	{
-
-        for (ch = 'a'; ch <= 'z'; ch++)
-        {
-                _putchar(ch);
-
-        }
-	num++;
-	_putchar('\n');
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		num++;
+		_putchar('\n');
 	}
-
 }
