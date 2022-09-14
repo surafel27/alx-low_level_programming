d73a4bcf130a6fb8e7c0419047e7c0e4e22ca4a1
surafel27 <surafelfekadu399@gmail.com>
2022-09-13 17:36:16 -0700

    low level programming

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,14 +8,13 @@
  */
 void print_to_98(int n)
 {
-	for (n = 0; n <= 98; n++)
+	for (n = 0; n < 98; n++)
 	{
 		printf("%d, ", n);
 	}
-	for (n = 0; n >= 98; n++)
+	for (n = 0; n > 98; n--)
 	{
 		printf("%d, ", n);
-		n--;
 	}
 	if (n == 98)
 		printf("%d, ", n);
