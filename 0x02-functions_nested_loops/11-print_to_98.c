5597e76f46c5471e57016de0ca82ecbf0594654a
surafel27 <surafelfekadu399@gmail.com>
2022-09-13 17:28:14 -0700

    low level programming

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_to_98 - prints the number from n to 98
@@ -10,11 +11,14 @@ void print_to_98(int n)
 {
 	for (n = 0; n <= 98; n++)
 	{
-		_putchar(n);
-		if (n < 98)
-		{
-			_putchar(',');
-			_putchar(' ');
-		}
+		printf("%d, ", n);
 	}
+	for (n = 0; n >= 98; n++)
+	{
+		printf("%d, ", n);
+		n--;
+	}
+	if (n == 98)
+		printf("%d, ", n);
+	printf("\n");
 }
