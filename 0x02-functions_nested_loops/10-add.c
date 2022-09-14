4744ef9ceb032e28c576ccd4dbd68d844d850f87
surafel27 <surafelfekadu399@gmail.com>
2022-09-13 17:11:46 -0700

    low level programming

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * add - the function adds the two number
- * @a: integer assigned as parameter
- * @b: integer assigned as parameter
  *
+ *@a: two integer arguments
+ *@b: two integer arguments
  * Return: sum
  */
 int add(int a, int b)
