e0db483fc41b7f27a3209c39625b180bacead8cc
surafel27 <surafelfekadu399@gmail.com>
2022-09-15 00:33:53 -0700

    low level programming

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,7 +12,7 @@
 
 void print_remanining_days(int month, int day, int year)
 {
-	if (year % 4 == 0 || ((year % 100 == 0) && ! (year % 400 == 0)))
+	if (year % 4 == 0 || ((year % 100 == 0) && (year % 400 == 0)))
 	{
 		if (month > 2 && day >= 60)
 		{
