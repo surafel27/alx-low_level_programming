32af066b488952d1c29eb8e59ae4c3d418c4f484
surafel27 <surafelfekadu399@gmail.com>
2022-09-15 01:22:44 -0700

    low level programming

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,20 +12,17 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && b > c)
+	if (a >= b && b >= c)
 	{
 		largest = a;
-		return (largest);
 	}
-	else if (b > a && a > c)
+	else if (b >= a && a >= c)
 	{
 		largest = b;
-		return (largest);
 	}
 	else
 	{
 		largest = c;
-		return (largest);
 	}
 	return (largest);
 }
