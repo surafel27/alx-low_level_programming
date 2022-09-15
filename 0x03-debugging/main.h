79048954003af6850ba2fcbf320aa9ce0c9dc6c0
surafel27 <surafelfekadu399@gmail.com>
2022-09-15 01:08:01 -0700

    low level programming

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,5 +1,5 @@
-#ifndef FILE_MAIN
-#define FILE_MAIN
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdio.h>
 
@@ -8,4 +8,4 @@ int largest_number(int a, int b, int c);
 int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
 
-#endif
+#endif /* MAIN_H*/
