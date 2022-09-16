c2b319fa492ab6cd05637b9138ebc79347a81e5c
surafel27 <surafelfekadu399@gmail.com>
2022-09-15 23:44:48 -0700

    low level programming

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,7 +14,11 @@ void more_numbers(void)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			_putchar(j + '0');
+			if (j >= 10)
+			{
+			_putchar(j / 10 + '0');
+			}
+			_putchar(i % 10 + '0');
 		}
 		_putchar('\n');
 	}
