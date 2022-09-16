9032869a5a10f791e9c803ad5613f08f70bf357b
surafel27 <surafelfekadu399@gmail.com>
2022-09-15 22:31:57 -0700

    low level programming

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - function used to print from 0 - 9
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	i = 48;
+	while (i <= 57)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
