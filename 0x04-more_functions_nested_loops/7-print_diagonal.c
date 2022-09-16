40ac9d0688f2dd5a89b59b87a643a7b13f71d14e
surafel27 <surafelfekadu399@gmail.com>
2022-09-16 03:25:03 -0700

    low level programming

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -4,7 +4,7 @@
 /**
  * print_diagonal - print diagonal line
  * @n: integer number of line '\'
- * Return: 0
+ * Return: void
  */
 
 void print_diagonal(int n)
@@ -13,13 +13,14 @@ void print_diagonal(int n)
 
 	for (row = 0; row < n; row++)
 	{
-		for (colum = 0; colum < n; colum++)
+		for (colum = 0; colum < row; colum++)
 		{
 			_putchar(' ');
 		}
 		_putchar('\\');
 		_putchar('\n');
 	}
+
 	if (n <= 0)
 	{
 		_putchar('\n');
