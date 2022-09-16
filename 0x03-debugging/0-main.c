8dfc1e52dbda124d416b49e57f1728132ffe619c
surafel27 <surafelfekadu399@gmail.com>
2022-09-15 20:53:53 -0700

    low level programming

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * main - tests function that prints if integer is positive or negative
- * Retun: 0
+ *main - Testes for posetive or negative
+ * Return: 0
  */
 
 int main(void)
@@ -11,6 +11,5 @@ int main(void)
 
 	i = 0;
 	positive_or_negative(i);
-
 	return (0);
 }
