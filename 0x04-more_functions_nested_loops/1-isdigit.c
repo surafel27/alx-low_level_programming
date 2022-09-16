9da93790a6db584f3955c0d3ccd8f10ca1754ac2
surafel27 <surafelfekadu399@gmail.com>
2022-09-15 22:06:55 -0700

    low level programming

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isdigit - checkes if the input is digit
+ * @c: integer value
+ * Return: 0
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	return (0);
+}
