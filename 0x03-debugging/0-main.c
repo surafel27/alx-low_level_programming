4226509acf196aa719c548256f211d6b9369ec15
surafel27 <surafelfekadu399@gmail.com>
2022-09-15 21:01:36 -0700

    low level programming

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *main - Testes for posetive or negative
+ * main - Testes for posetive or negative
  * Return: 0
  */
 
@@ -9,7 +9,7 @@ int main(void)
 {
 	int i;
 
-	i = 98;
+	i = 0;
 	positive_or_negative(i);
 	return (0);
 }
