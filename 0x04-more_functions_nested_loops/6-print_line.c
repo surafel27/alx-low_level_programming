9449b07ee128598f9e14f87a85a2049f5e10ea0b
surafel27 <surafelfekadu399@gmail.com>
2022-09-16 01:14:40 -0700

    low level programming

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * print_line - prints line as output
  * @n: integer parameter
--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_square - print square of the size
