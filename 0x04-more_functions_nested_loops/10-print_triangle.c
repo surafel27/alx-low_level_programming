9b00389ed2a494229ccb3ec4a9b0b0fe35df2a20
surafel27 <surafelfekadu399@gmail.com>
2022-09-16 02:25:57 -0700

    low level programming

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_triangle - function to print triangle
+ * @size: print # for the integer
+ * Result: print triangle
+ */
+
+void print_triangle(int size)
+{
+	int i, j, tab;
+
+	for (i = 0; i <= size; i++)
+	{
+		for (tab = size - 1; tab > 0; tab--)
+		{
+			_putchar(' ');
+		}
+		for (j = 0; j <= i; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+}
