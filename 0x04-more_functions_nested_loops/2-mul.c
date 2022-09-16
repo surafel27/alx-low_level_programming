1e7a8a64360917b844bb996579cc57fce9af124a
surafel27 <surafelfekadu399@gmail.com>
2022-09-15 22:22:14 -0700

    low level programming

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two number
+ * @a: integer value
+ * @b: integer value
+ * Return: multiple of numbers
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
