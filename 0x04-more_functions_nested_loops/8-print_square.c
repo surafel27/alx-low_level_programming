e2c81ac4eaf6796e97497e4587df6b1a164e2c08
surafel27 <surafelfekadu399@gmail.com>
2022-09-16 00:38:55 -0700

    low level programming

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_square - print square of the size
+ * @size: integer size of the square
+ * Return: 0
+ */
+
+void print_square(int size)
+{
+	int row, colum;
+
+	for (row = 0; row <= size; row++)
+	{
+		for (colum = 0; colum <= size; colum++)
+		{
+			_putchar(35);
+		}
+		_putchar('\n');
+	}
+}
