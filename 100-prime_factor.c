b8b5f923c90637665a828bde0932e9918398151c
surafel27 <surafelfekadu399@gmail.com>
2022-09-16 14:29:38 -0700

    low level programming

--- 100-prime_factor.c
+++ 100-prime_factor.c
@@ -1,25 +1,21 @@
 #include <stdio.h>
-
 /**
- * main -  calculate largest prime of 612852475143
- *@void: no argument
- * Return: Success Always
- */
-
-
+* main - entry block
+* @void: no argument
+* Return: 0
+*/
 int main(void)
 {
-	long int x = 612852475143;
-	long int py;
+	long i, number = 612852475143;
 
-	for (py = 2; py <= x; py++)
+	for (i  = 2; i <= number; i++)
 	{
-		if (x % py == 0)
+		if (number % i == 0)
 		{
-			x = x / py;
-			py--;
+			number = number / i;
+			i--;
 		}
 	}
-	printf("%ld\n", py);
+	printf("%lu\n", i);
 	return (0);
 }
