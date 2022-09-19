cdf02764ee4f085152535c8a47dabf4b49fbde50
surafel27 <surafelfekadu399@gmail.com>
2022-09-19 06:10:41 -0700

    low level programming

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts2 - print every character of string
+ * @str: pointer to character
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int a, b = 0;
+
+	while (str[a] != '\0')
+	{
+		a++;
+	}
+	while (b < a)
+	{
+		_putchar(str[b]);
+	}
+	_putchar('\n');
+}
