acc1dae397074e2f8b8418a4762c0373cacf7def
surafel27 <surafelfekadu399@gmail.com>
2022-09-19 11:47:58 -0700

    low level programming

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcpy - function copies sring
+ * @dest: character pointer
+ * @src: character pointer
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+	return (dest);
+}
