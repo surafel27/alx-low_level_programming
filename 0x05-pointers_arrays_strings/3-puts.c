0f15e6678655684e9967e2a0cca015b503ec8847
surafel27 <surafelfekadu399@gmail.com>
2022-09-19 03:28:35 -0700

    fuction to print string using puts();

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+#include <stdout.h>
+
+/**
+ * _puts - fuction to print a string
+ * @str: character pointer
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+}
