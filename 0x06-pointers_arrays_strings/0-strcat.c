53e074ffb918fe30fca4a148b61eedc19d707fda
surafel27 <surafelfekadu399@gmail.com>
2022-09-21 23:25:19 -0700

    low level programming

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,10 +12,10 @@ char *_strcat(char *dest, char *src)
 	int i, dest_len;
 
 	dest_len = strlen(dest);
-	for (i = 0; i < (src[i] != '\0'); i++)
+	for (i = 0; src[i] != '\0'; i++)
 	{
 		dest[dest_len + i] = src[i];
 	}
-	dest[dest_len] = '\0';
+	dest[dest_len + i] = '\0';
 	return (dest);
 }
