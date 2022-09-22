df1d70dded7538d613775b926f5eeb8a046be240
surafel27 <surafelfekadu399@gmail.com>
2022-09-21 23:43:44 -0700

    low level programming

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,7 +13,6 @@ char *_strncat(char *dest, char *src, int n)
 	int i, dest_len;
 
 	dest_len = strlen(dest);
-	n = strlen(src);
 	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[dest_len + i] = src[i];
