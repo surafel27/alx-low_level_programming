e3464cf8892b1f93e575e193ea53ddf9b2204929
surafel27 <surafelfekadu399@gmail.com>
2022-09-26 05:47:42 -0700

    low level programming

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,11 +11,13 @@ char *_strchr(char *s, char c)
 {
 	while (*s != '\0')
 	{
-		if (*s == 'c')
+		if (*s == c)
 		{
 			return (s);
 		}
+		else
+			return (NULL);
 		s++;
 	}
-	return (NULL);
+	return (s);
 }
