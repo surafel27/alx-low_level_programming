8ad354ae5875d85611e3a4fe1bc60c284640eecb
surafel27 <surafelfekadu399@gmail.com>
2022-09-28 06:21:24 -0700

    low level programming

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - fuction used to calculate square root
+ * @n: integer to be squared
+ * Return: integer sqrt
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 1)
+	{
+		return (1);
+	}
+	return (_sqrt_recursion(n * 1 / 2));
+}
