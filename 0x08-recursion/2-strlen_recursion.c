0728e7cdeb930f47a1fe6cb2147bb18486c5ab09
surafel27 <surafelfekadu399@gmail.com>
2022-09-28 05:10:55 -0700

    string length

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,12 +8,9 @@
 
 int _strlen_recursion(char *s)
 {
-	int len = 0;
-
-	if (*s != '\0')
+	if (*s == '\0')
 	{
-		_strlen_recursion(s + 1);
-		len++;
+		return (0);
 	}
-	return (len);
+	return (_strlen_recursion(s + 1) + 1);
 }
