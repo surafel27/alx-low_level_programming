ba18a7e21cc870468455a7f3a88f7800a0bd4da6
surafel27 <surafelfekadu399@gmail.com>
2022-09-28 04:14:30 -0700

    low level programming

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,10 +8,13 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s != '\0')
+	int i = 0;
+
+	if (s[i] != '\0')
 	{
 		_putchar(*s);
-		_puts_recursion(s + 1);
+		_puts_recursion(s + i);
+		i++;
 	}
 	 _putchar('\n');
 }
