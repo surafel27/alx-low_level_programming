874194db0cc1d3253ea4b8a759e63287ca8e4de2
surafel27 <surafelfekadu399@gmail.com>
2022-09-30 05:03:25 -0700

    argc and argv[]

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - fuction print the number of arhument passed
+ * @argc: the size of the argument
+ * @argv: the array size of the argument
+ * Return: Always (0)
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("argc = %d", argc);
+	_putchar('\n');
+	return (0);
+}
