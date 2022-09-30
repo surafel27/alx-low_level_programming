701c3ccf78773655f9eb836a343de093edb8e60e
surafel27 <surafelfekadu399@gmail.com>
2022-09-30 05:17:17 -0700

    argc and argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,1 +1,13 @@
 #include "main.h"
+
+/**
+ * main - fuction used tor pting its name
+ * @argc: counte the size
+ * @argv: the size of argc
+ * Return: Alwayes zero
+ */
+int main(int argc, char **argv)
+{
+	_putchar(argv[0]);
+	return (0);
+}
--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,7 +7,7 @@
  * Return: Always (0)
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
 	printf("argc = %d", argc);
 	_putchar('\n');
