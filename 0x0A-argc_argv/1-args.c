74145e4491dd32b021fd80eb83647631e69c9660
surafel27 <surafelfekadu399@gmail.com>
2022-09-30 05:29:20 -0700

    argc and argv

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * main - fuction print the number of arhument passed
@@ -7,9 +8,8 @@
  * Return: Always (0)
  */
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	printf("argc = %d", argc);
-	_putchar('\n');
+	printf("argc = %d\n", argc);
 	return (0);
 }
