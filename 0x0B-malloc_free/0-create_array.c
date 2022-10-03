f6a1fbebf1ba56ef70b5d477ba5f26008655af8d
surafel27 <surafelfekadu399@gmail.com>
2022-10-03 06:27:47 -0700

    C is fun

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - fuction used to create arrt of string
+ * @size: size of the character
+ * @c: character
+ * Return: ponter of array
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	int i;
+	char *str;
+
+	str = (char *)malloc((size + 1) * sizeof(char));
+
+	if (str == 0)
+	{
+		return (NULL);
+	}
+	i = 0;
+	while (i < size)
+	{
+		str[i] = c;
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
