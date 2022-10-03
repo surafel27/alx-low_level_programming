45782e7953b226fb5449c39cbd43f47d9495e146
surafel27 <surafelfekadu399@gmail.com>
2022-10-03 06:31:54 -0700

    C is fun

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,7 +14,7 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *str;
 
-	str = (char *)malloc((size + 1) * sizeof(char));
+	str = (char *)malloc((size) * sizeof(char));
 
 	if (str == 0)
 	{
