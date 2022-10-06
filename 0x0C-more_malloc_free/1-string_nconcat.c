a3d662842ffb1e76fa2e8e9d60b0e46d183c613f
surafel27 <surafelfekadu399@gmail.com>
2022-10-06 03:07:24 -0700

    C is fun

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,6 +15,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *str;
 	unsigned int len, i, j, a;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	for (i = 0; s1[i] != '\0'; i++)
 		;
 	for (j = 0; j < n; j++)
