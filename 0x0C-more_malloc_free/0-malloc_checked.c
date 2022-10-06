5cd1050703a7200312effc83336140171e31ee57
surafel27 <surafelfekadu399@gmail.com>
2022-10-05 23:42:34 -0700

    c is na fun

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - fuction allocate memory
+ * @b: integer a memory to be allocated
+ * Return: pointer to allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	unsigned int *a;
+
+	a = malloc(sizeof(b));
+	if (a == NULL)
+	{
+		return ((void *)98);
+	}
+	return (a);
+}
