a0e0f747c910c92682f809ecb5bf6135d921607d
surafel27 <surafelfekadu399@gmail.com>
2022-10-06 05:41:32 -0700

    C is na fun :(

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _calloc - function allocate memory for an array
+ * @nmemb: integer
+ * @size: integer
+ * Return: array
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i, mul;
+	char *num;
+
+	mul = nmemb * size;
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	num = malloc(mul);
+	if (num == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < mul; i++)
+	{
+		num[i] = 0;
+	}
+	return (num);
+}
