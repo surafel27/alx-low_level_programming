ea9f98c1fe409caabf38d926d3f2eecb4c3f384f
surafel27 <surafelfekadu399@gmail.com>
2022-10-10 12:26:07 -0700

    C is not fun

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,20 +11,27 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 	{
-		return(" ");
+		return;
 	}
 	if (d->name == NULL)
 	{
-		printf("Name: (nil)\n");
+		d->name = "(nil)";
 	}
 	else
 	{
 		printf("Name: %s\n", d->name);
+	}
+	if (d->age <= 0)
+	{
+		printf("Age: (nil)\n");
+	}
+	else
+	{
 		printf("Age: %.6f\n", d->age);
 	}
 	if (d->owner == NULL)
 	{
-		printf("Owner: (nil)\n");
+		d->owner = "(nil)";
 	}
 	else
 	{
