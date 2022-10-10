237534ecbfb7dead29c28e3fdb10d219b09bc393
surafel27 <surafelfekadu399@gmail.com>
2022-10-10 04:13:46 -0700

    structure in C progarmming

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,18 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - fuction 
+ * @name: string
+ * @age: float
+ * @owner: string
+ * Description: a new type that contains dog name, age, and owner.
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
+}dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
