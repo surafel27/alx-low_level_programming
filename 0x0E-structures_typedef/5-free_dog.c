69c333f2c00dc1fc2b8b7e2d5a30c98ef8376178
surafel27 <surafelfekadu399@gmail.com>
2022-10-10 06:18:47 -0700

    structure in c programming

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - fuction frees the memory
+ * @d: typedef of structure
+ * Return: void
+ */
+void free_dog(dog_t *d)
+{
+	if(d)
+	{
+		free(d->name);
+		free(d->age);
+		free(d->owner);
+		free(d);
+	}
+}
