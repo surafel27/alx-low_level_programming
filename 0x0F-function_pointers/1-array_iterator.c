ad5b6f34f18b9a7757e9f96ea06679224ddb0418
surafel27 <surafelfekadu399@gmail.com>
2022-10-12 01:12:23 -0700

    function pointer is crazy

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * array_iterator - fuction iterate in an array
+ * @array: pointer to an array of integer
+ * @size: the size of the array
+ * @action: is a pointer to a function
+ * Return: void/nothing
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
