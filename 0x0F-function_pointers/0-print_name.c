64a0e2c79826e108b9ad2e05cdb4bce01539fbe6
surafel27 <surafelfekadu399@gmail.com>
2022-10-12 00:41:52 -0700

    function pointer is crazy

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * print_name - fuction used to print name
+ * @name: character pointer
+ * @f: function pointer
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
