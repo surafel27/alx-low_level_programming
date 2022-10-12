aa732f5fd83aef08542227e56bfd80dbe554f0e6
surafel27 <surafelfekadu399@gmail.com>
2022-10-12 05:26:13 -0700

    fuction pointer in c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,31 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * int_index - fuction search for integer
+ * @array: an array of integer
+ * @size: size of the array
+ * @cmp: function pointer
+ * Return: number of occurance
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+	if (cmp != NULL && array != NULL)
+	{
+		for (i = 0; i < size; i++)
+		{			
+			if (cmp(array[i]) != 0)
+			{
+				return (i);
+			}
+		}
+	}
+	return (-1);
+}
