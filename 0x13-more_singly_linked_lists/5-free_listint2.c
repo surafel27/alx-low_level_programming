04cbc07f351c078b1961ef7fea20b336fbbd88a2
surafel27 <surafelfekadu399@gmail.com>
2022-10-24 10:29:54 -0700

    more on singly linked list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -17,8 +17,8 @@ void free_listint2(listint_t **head)
 		listint_t *temp;
 
 		temp = *head;
-		*head = temp->next;
+		(*head) = (*head)->next;
 		free(temp);
 	}
-	*head = NULL;
+	(*head) = NULL;
 }
