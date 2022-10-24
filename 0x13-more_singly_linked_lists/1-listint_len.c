3dcca0a54e7c31b8ed121cff9c943c1952d9496f
surafel27 <surafelfekadu399@gmail.com>
2022-10-24 10:09:48 -0700

    more on singly linked list

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -8,16 +8,16 @@
  */
 size_t listint_len(const listint_t *h)
 {
-        int count = 0;
+	int count = 0;
 
-        if (h == NULL)
-        {
-                return (0);
-        }
-        while (h != NULL)
-        {
-                count += 1;
-                h = h->next;
-        }
-        return (count);
+	if (h == NULL)
+	{
+		return (0);
+	}
+	while (h != NULL)
+	{
+		count += 1;
+		h = h->next;
+	}
+	return (count);
 }
