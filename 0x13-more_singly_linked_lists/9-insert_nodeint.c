9f9b4d6cc0aa9890eb3f08ce6fd0eb983e82390e
surafel27 <surafelfekadu399@gmail.com>
2022-10-25 02:28:30 -0700

    more fix on singly linked list

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -43,6 +43,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		tmp = tmp->next;
 		i++;
 	}
+	if(idx == 0)
+	{
+		newnode->next = *head;
+		*head = newnode;
+		return (newnode);
+	}
 	if (i == idx)
 	{
 		newnode->next = tmp->next;
