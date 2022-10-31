30c6b57c76d647cd65d3c77b630185436e4447e8
surafel27 <surafelfekadu399@gmail.com>
2022-10-31 11:56:45 -0700

    file i/o project

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,15 +10,15 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, i;
+	int fd, wr, i = 0;
 
 	if (filename == NULL)
 		return (-1);
 	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 	if (fd == -1)
 		return (-1);
-	for (i = 0; text_content[i]; i++)
-		;
+	while (text_content[i])
+		i++;
 	if (text_content == NULL)
 	{
 		close(fd);
@@ -26,8 +26,10 @@ int create_file(const char *filename, char *text_content)
 	}
 	else
 	{
-		write(fd, text_content, i);
+		wr = write(fd, text_content, i);
 	}
+	if (wr == -1)
+		return (-1);
 	close(fd);
 
 	return (1);
