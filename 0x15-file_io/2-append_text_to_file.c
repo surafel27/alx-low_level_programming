e56cc128a35c461b2af908f2918323f5db0d3a27
surafel27 <surafelfekadu399@gmail.com>
2022-11-01 11:18:43 -0700

    fight until the end

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -25,7 +25,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		close(fd);
 		return (1);
 	}
-	for (i = 0; text_conetnt[i] != '\0'; i++)
+	for (i = 0; text_content[i] != '\0'; i++)
 		;
 	fwr = write(fd, text_content, i);
 	if (fwr == -1 || fwr != i)
