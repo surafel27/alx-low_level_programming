d861a5e9651a811c4263a85c4577cb84327ff346
surafel27 <surafelfekadu399@gmail.com>
2022-11-01 11:02:04 -0700

    fight until the end

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 	while (text_content[i] != '\0')
@@ -26,7 +26,10 @@ int create_file(const char *filename, char *text_content)
 	}
 	wr = write(fd, text_content, i);
 	if (wr == -1 || wr != i)
+	{
+		close(fd);
 		return (-1);
+	}
 	close(fd);
 	return (1);
 }
