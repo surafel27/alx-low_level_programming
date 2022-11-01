fb4cd6c23251e0fa0c1bbcc433aa8730acb4af72
surafel27 <surafelfekadu399@gmail.com>
2022-11-01 11:16:52 -0700

    fight until the end

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,29 +11,28 @@
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd, fwr;
-	int i = 0;
+	int i;
 
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_RDWR | O_APPEND);
+	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
-
-	while (text_content[i])
-		i++;
 
 	if (text_content == NULL)
 	{
 		close(fd);
-		return (-1);
+		return (1);
 	}
-	else
+	for (i = 0; text_conetnt[i] != '\0'; i++)
+		;
+	fwr = write(fd, text_content, i);
+	if (fwr == -1 || fwr != i)
 	{
-		fwr = write(fd, text_content, i);
-	}
-	if (fwr == -1)
+		close(fd);
 		return (-1);
+	}
 	close(fd);
 	return (1);
 }
