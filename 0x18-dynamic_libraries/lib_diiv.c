69d5ff2669baa8dfd58ab4ee8f52bf720f7b37ce
surafel27 <surafelfekadu399@gmail.com>
2022-12-12 02:34:21 -0800

    this time push the python to C

---
+++ 0x18-dynamic_libraries/lib_diiv.c
@@ -0,0 +1,6 @@
+#include "main.h"
+
+int diiv(int a, int b)
+{
+	return (a / b);
+}
--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -25,4 +25,9 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
+int add(int a, int b);
+int sub(int a, int b);
+int diiv(int a, int b);
+int mul(int a, int b);
+int mod(int a, int b);
 #endif
