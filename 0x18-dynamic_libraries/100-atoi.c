8393e76cf812033a89032feebe901bcd14e29559
surafel27 <surafelfekadu399@gmail.com>
2022-12-12 01:57:40 -0800

    fix issue

--- 0x18-dynamic_libraries/100-atoi.c
+++ 0x18-dynamic_libraries/100-atoi.c
@@ -1,11 +1,30 @@
 #include "main.h"
 
 /**
- * _atoi - Entry point
- * @s: input
- * Return: Always 0 (Success)
+ * _atoi - function convert sring to s
+ * @s: character pointer
+ * Return: integer value
  */
 int _atoi(char *s)
 {
-	return (0);
+	int i;
+	int res = 0;
+	int sig = -1;
+	int brk = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == '-')
+			sig = sig * -1;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			res = res * 10;
+			res -= (s[i] - '0');
+			brk = 1;
+		}
+		else if (brk == 1)
+			break;
+	}
+	res = sig * res;
+	return (res);
 }
